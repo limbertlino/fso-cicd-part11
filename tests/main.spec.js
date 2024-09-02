edee7c08c85e280687c9a95dfd896824276d833e
Limbert Lino <limbertlino@gmail.com>
2024-09-01 20:02:12 -0400

    Second e2e test added

---
+++ tests/main.spec.js
@@ -0,0 +1,23 @@
+const { test, describe, expect } = require('@playwright/test')
+
+describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('http://localhost:8080/')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(
+      page.getByText(
+        'Pokémon and Pokémon character names are trademarks of Nintendo.'
+      )
+    ).toBeVisible()
+  })
+  test('can navigate to ivysaur page and see chlorophyll ability', async ({
+    page,
+  }) => {
+    await page.goto('http://localhost:8080/')
+    await page.click('text=ivysaur')
+    await expect(page).toHaveURL('http://localhost:8080/pokemon/ivysaur')
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
+})
+
+describe('Pokedex', () => {})
